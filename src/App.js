8a134e4795cc1be0febc02f9a8162f820185f243
feiybox <feiybox@foxmail.com>
2021-09-30 22:04:14 +0800

    source code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import {Button, Popover} from 'antd';
+import names from "./constant";
+import {QuestionCircleFilled, GithubOutlined} from '@ant-design/icons';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <div>
+                    <div id={"desc"}>选择一个你满意的名称 <span/>
+                        <Popover placement="left" color={"#282c34"} content={content}>
+                            <QuestionCircleFilled id={"question-icon"} className={"question-icon"}/>
+                        </Popover>
+                    </div>
+                    <p/>
+                    <div id={"name"} className={"name_content"}>...</div>
+                    <p/>
+                    <Button id={"choose"} type="primary" size="large" shape="round"
+                            onClick={() => choose()}>选择一个</Button>
+                    <p/>
+                    <div id={"ph"}></div>
+                </div>
+                <div className={"App-bottom"}>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <Button type="link"
+                            href="https://github.com/feiybox/aiden/issues/new"><GithubOutlined/> 有好的名称或者建议，推荐给我吧</Button>
+                </div>
+            </header>
+        </div>
+    );
 }
+
+function choose() {
+    let t = document.getElementById("name").innerText
+    let ns = names.copyWithin(0, 0).filter((value) => {
+        return value !== t
+    })
+
+    let index = parseInt(Math.random() * ns.length)
+    document.getElementById("name").innerText = ns[index]
+    document.getElementById("choose").innerText = "重新选择"
+    document.getElementById("ph").innerText = "满意么？用它给你的项目 / 产品 / 平台命名吧！"
+}
+
+const content = (
+    <div className={"App-popover"}>
+        <div>选择一个满意名称有什么用？</div>
+        <div>在工作中</div>
+        <div>会遇到很多项目、平台、产品，都有着有意思的名称</div>
+        <div>当我们的平台需要一个有辨识度又很有寓意的名称时</div>
+        <div>很多时候</div>
+        <div>我们想不到比较好的名称</div>
+        <div>那怎么办？</div>
+        <div>试一下这儿的名称吧</div>
+        <div>可以</div>
+        <div>直接使用</div>
+        <div>也可以</div>
+        <div>给你一些灵感</div>
+    </div>
+);
 
 export default App;
---
+++ src/constant.js
@@ -0,0 +1,7 @@
+const names = [
+    "太平洋", "印度洋", "大西洋", "北冰洋",
+    "嫦娥", "月兔",
+    "Tesla",
+]
+
+export default names;
